04644906c4ea2d3df6abdb72341e76479ef483b5
Aras <114133924+Dirigentas@users.noreply.github.com>
2023-06-08 21:06:05 +0300

    Twig syntax

---
+++ notes.md
@@ -0,0 +1,12 @@
+# Eiga:
+
+-   Set-ExecutionPolicy RemoteSigned - Scope CurrentUser
+-   irm get.scoop.sh | iex
+-   scoop install symfony-cli
+-   symfony new mixed_vinyl
+-   symfony serve -d
+-   composer require templates
+
+git status
+git add .
+git coomit -m "zinute"
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,13 +4,27 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        // return new Response('Title: PB and Jams');
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB and Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{genre}')]
